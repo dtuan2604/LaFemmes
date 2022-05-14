1b34d365e758f019359124d304f954b878144267
tdhhxn <tdhhxn@umsystem.edu>
2022-05-13 23:28:00 -0500

    Updates

---
+++ precache-manifest.874d622515887cf14af5d33c952d2883.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cd36628ee2bba0c4107",
+    "url": "/LaFemmes/static/css/main.5d292913.chunk.css"
+  },
+  {
+    "revision": "8cd36628ee2bba0c4107",
+    "url": "/LaFemmes/static/js/main.7f6a1d2d.chunk.js"
+  },
+  {
+    "revision": "a5e6a7876cda5270bd9d",
+    "url": "/LaFemmes/static/js/runtime~main.ed7602b7.js"
+  },
+  {
+    "revision": "2578e91f12891fc3e79a",
+    "url": "/LaFemmes/static/css/2.07dfb050.chunk.css"
+  },
+  {
+    "revision": "2578e91f12891fc3e79a",
+    "url": "/LaFemmes/static/js/2.e205d9ad.chunk.js"
+  },
+  {
+    "revision": "7448a7619c8785bdc2eb8727a877724c",
+    "url": "/LaFemmes/static/media/logo.7448a761.png"
+  },
+  {
+    "revision": "c06e192889e5b999073af01deb4fc039",
+    "url": "/LaFemmes/index.html"
+  }
+];
